9b5e7b8d98f0ba1e2fcc0ff29d7826d784c015aa
Adriano <adriano_alves_2008@hotmail.com>
2022-11-19 15:21:34 -0300

    fazendo a lógica da busca

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express()
 const path = require('path')
 const db = require('./db/connection')
 const bodyParser = require('body-parser')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 
 const Job = require('./models/Job')
 
@@ -35,11 +37,30 @@ db.authenticate()
 
 //routes
 app.get('/', (req, res) => {
-  Job.findAll({ order: [['createdAt', 'DESC']] }).then(jobs => {
-    res.render('index', {
-      jobs
+  let search = req.query.job
+  let query = '%' + search + '%'
+
+  if (!search) {
+    Job.findAll({ order: [['createdAt', 'DESC']] })
+      .then(jobs => {
+        res.render('index', {
+          jobs
+        })
+      })
+      .catch(err => console.log(err))
+  } else {
+    Job.findAll({
+      where: { title: { [Op.like]: query } },
+      order: [['createdAt', 'DESC']]
     })
-  })
+      .then(jobs => {
+        res.render('index', {
+          jobs,
+          search
+        })
+      })
+      .catch(err => console.log(err))
+  }
 })
 
 //jobs routes
